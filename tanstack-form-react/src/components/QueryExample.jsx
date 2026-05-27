e5237a8e0e5c12843aab08b9679ce2905d4d73c2
Visshnnu Tejaa <visshnnutejaa@gmail.com>
2026-05-27 07:14:27 +0530

    refactor QueryExample component to improve error handling and enable post loading

--- tanstack-form-react/src/components/QueryExample.jsx
+++ tanstack-form-react/src/components/QueryExample.jsx
@@ -10,22 +10,30 @@ async function fetchData() {
 }
 
 const QueryExample = () => {
+  const [isLoadPosts, setLoadPosts] = useState(false);
   const {
     data: posts,
-    isError,
+    error,
     isLoading,
+    isError, // if any error is present
+    isEnabled, // is Lazy loading of the data to any button is enabled or not
+    isSuccess, // if the query was success
+    isStale, // if the query went to stale
+    status, // ! Also you will get all these info in a single variable called status
   } = useQuery({
     queryKey: ["posts"],
     queryFn: fetchData,
+    enabled: isLoadPosts,
   });
 
   if (isLoading) return <p>Loading...</p>;
-  if (isError) return <p>Something went wrong</p>;
+  if (error) return <p>Something went wrong</p>;
 
   return (
     <div>
       <h2>1. Intro and setup</h2>
       <p>This is our first query without tanstack query</p>
+      <button onClick={() => setLoadPosts(true)}>Load Posts</button>
       {posts &&
         posts.map((post) => (
           <div>
