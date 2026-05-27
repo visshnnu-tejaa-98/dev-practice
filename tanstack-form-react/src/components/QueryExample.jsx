5105b0cb1aab2296dd8dcc4148571f392fde1954
Visshnnu Tejaa <visshnnutejaa@gmail.com>
2026-05-27 07:06:54 +0530

    refactor QueryExample component to use useQuery for data fetching

--- tanstack-form-react/src/components/QueryExample.jsx
+++ tanstack-form-react/src/components/QueryExample.jsx
@@ -1,28 +1,38 @@
+import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 
-const QueryExample = () => {
-  const [posts, setPosts] = useState([]);
+async function fetchData() {
+  const res = await fetch(
+    "https://jsonplaceholder.typicode.com/posts?_limit=5",
+  );
+  const data = await res.json();
+  return data;
+}
 
-  useEffect(() => {
-    async function fetchData() {
-      const res = await fetch(
-        "https://jsonplaceholder.typicode.com/posts?_limit=5",
-      );
-      const data = await res.json();
-      setPosts(data);
-    }
-    fetchData();
+const QueryExample = () => {
+  const {
+    data: posts,
+    isError,
+    isLoading,
+  } = useQuery({
+    queryKey: ["posts"],
+    queryFn: fetchData,
   });
+
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p>Something went wrong</p>;
+
   return (
     <div>
       <h2>1. Intro and setup</h2>
       <p>This is our first query without tanstack query</p>
-      {posts.map((post) => (
-        <div>
-          <h4>{post.title}</h4>
-          <p>{post.body}</p>
-        </div>
-      ))}
+      {posts &&
+        posts.map((post) => (
+          <div>
+            <h4>{post.title}</h4>
+            <p>{post.body}</p>
+          </div>
+        ))}
     </div>
   );
 };
