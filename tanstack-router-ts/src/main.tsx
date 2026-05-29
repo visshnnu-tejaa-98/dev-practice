2ae13643ed75aebb0ff2378f34e49b016f713273
Visshnnu Tejaa <visshnnutejaa@gmail.com>
2026-05-29 19:32:09 +0530

    Implement tanstack router

--- tanstack-router-ts/src/main.tsx
+++ tanstack-router-ts/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
 
-createRoot(document.getElementById('root')!).render(
+const router = createRouter({
+  routeTree,
+  defaultPreload: "intent",
+  scrollRestoration: true,
+});
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
