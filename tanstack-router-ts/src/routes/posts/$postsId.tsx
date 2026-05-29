79d3c0a5e11a4064de378901dd718fd1fe5b2eb0
Visshnnu Tejaa <visshnnutejaa@gmail.com>
2026-05-29 21:21:49 +0530

    Accessing param in dynamic page

--- tanstack-router-ts/src/routes/posts/$postsId.tsx
+++ tanstack-router-ts/src/routes/posts/$postsId.tsx
@@ -2,8 +2,17 @@ import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/posts/$postsId")({
   component: RouteComponent,
+  loader: async ({ params }) => {
+    // Here you can fetch something from db using params
+    // Later we can return the response and we can add it in Actual COmpoenent
+    return {
+      postId: params.postsId,
+    };
+  },
 });
 
 function RouteComponent() {
-  return <div>Hello "/posts/$postsId"!</div>;
+  const { postId } = Route.useLoaderData();
+
+  return <div>Hello "/posts/$postsId"! - ${postId}</div>;
 }
