cb7c8226623a449d2f82362348caa0e6df3fb026
Visshnnu Tejaa <visshnnutejaa@gmail.com>
2026-05-30 06:52:14 +0530

    Navigate Programatically using useNavigate hook

--- tanstack-router-practice/src/routes/contact.tsx
+++ tanstack-router-practice/src/routes/contact.tsx
@@ -1,13 +1,17 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/contact")({
   component: Contact,
 });
 
 function Contact() {
+  const navigate = useNavigate();
   return (
     <div>
       <h1>Contact Page</h1>
+      <button onClick={() => navigate({ to: "/about" })}>
+        Navigate to About
+      </button>
     </div>
   );
 }
