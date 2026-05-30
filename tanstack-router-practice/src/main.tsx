ddbc0d863647526cc97e1eb5d36111e8ca5f8041
Visshnnu Tejaa <visshnnutejaa@gmail.com>
2026-05-30 07:29:33 +0530

    Add 404 Page

--- tanstack-router-practice/src/main.tsx
+++ tanstack-router-practice/src/main.tsx
@@ -1,14 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { createRouter, Link, RouterProvider } from "@tanstack/react-router";
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 // Create a new router instance
-const router = createRouter({ routeTree });
+const router = createRouter({
+  routeTree,
+  defaultNotFoundComponent: () => (
+    <div>
+      <h1>Sorry!, this Page does not exist</h1>
+      <Link to="/">Home page</Link>
+    </div>
+  ),
+});
 
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
