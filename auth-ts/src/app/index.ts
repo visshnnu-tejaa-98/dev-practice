081842897cbd58db1d84ee17a372882d29463e9f
Visshnnu Tejaa <visshnnutejaa@gmail.com>
2026-06-01 16:21:01 +0530

    Refactor Authroute code

--- auth-ts/src/app/index.ts
+++ auth-ts/src/app/index.ts
@@ -1,27 +1,8 @@
 import express from "express";
-import type { Request, Response } from "express";
 import ApiResponse from "./common/utils/api-response";
-import {
-  ConflictError,
-  NotFoundError,
-  ValidationError,
-} from "./common/utils/api-error";
+import { NotFoundError } from "./common/utils/api-error";
 import { errorMiddleWare } from "./common/utils/error.middleware";
-import { registerSchema } from "./module/auth/auth.schema";
-import { validateRegisterInputData } from "./module/auth/auth.validation";
-import db from "../db";
-import { usersTable } from "../db/schema";
-import { eq } from "drizzle-orm";
-import { checkUserWithEmailExists, insertUser } from "./module/auth/auth.utils";
-import bcrypt from "bcryptjs";
-import {
-  generateSalt,
-  generateVerifyEmailToken,
-  hash,
-  hashToken,
-} from "./common/utils/jwt";
-import { sendVerificationEmail } from "./common/config/nodemailer";
-import { registerUser } from "./module/auth/auth.controller";
+import AuthRouter from "./module/auth/auth.route";
 
 const createExpressApp = () => {
   const app = express();
@@ -33,7 +14,7 @@ const createExpressApp = () => {
     return ApiResponse.success(res, "API is healthy");
   });
 
-  app.post("/api/auth/register", registerUser);
+  app.use("/api/auth", AuthRouter);
 
   app.use((req, res, next) => {
     next(new NotFoundError(`Route ${req.originalUrl} not found`));
