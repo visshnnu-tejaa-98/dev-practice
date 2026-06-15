4af6ab0083d4f4cbf23fcf2a86c068a9608b1784
Visshnnu Tejaa <visshnnutejaa@gmail.com>
2026-06-15 14:31:17 +0530

    Implement sample UI and add a send message handler

--- socket-crash-course/index.js
+++ socket-crash-course/index.js
@@ -1,13 +1,19 @@
-import http, { createServer } from "node:http";
+import { createServer } from "node:http";
 import { Server } from "socket.io";
+import path from "path";
+import express from "express";
+import { fileURLToPath } from "node:url";
 
 async function main() {
-  const server = createServer();
+  const app = express();
+  app.use(express.static(path.resolve("./public")));
+
+  const server = createServer(app);
   const io = new Server();
 
   io.attach(server);
 
-  const PORT = process.env.PORT || 6000;
+  const PORT = process.env.PORT || 9002;
   server.listen(PORT, () =>
     console.log(`🚀 Server is up and running in PORT ${PORT}`),
   );
