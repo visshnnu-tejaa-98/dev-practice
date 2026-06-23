2b87454e2e7dd943653a0d23a9a485cc16725612
Visshnnu Tejaa <visshnnutejaa@gmail.com>
2026-06-23 07:48:33 +0530

    Create Organisation Switcher

--- multi-tenent-app-clerk/app/components/nav.tsx
+++ multi-tenent-app-clerk/app/components/nav.tsx
@@ -1,12 +1,17 @@
-import { UserButton } from "@clerk/nextjs";
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 import * as React from "react";
 
 const Nav: React.FC = () => {
   return (
     <nav>
-      <div className="p-4 flex justify-between items-center">
-        <h1 className="font-semibold text-xl">Blog Application</h1>
-        <UserButton />
+      <div className="p-4 flex justify-between items-center bg-[#eeeeee]">
+        <div>
+          <h1 className="font-semibold text-xl">Blog Application</h1>
+        </div>
+        <div className="flex gap-2 justify-center items-center">
+          <OrganizationSwitcher />
+          <UserButton />
+        </div>
       </div>
       <div></div>
     </nav>
