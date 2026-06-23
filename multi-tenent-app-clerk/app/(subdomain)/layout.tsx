fcc98b8af922066037fdc9598dac18de2de6c0ed
Visshnnu Tejaa <visshnnutejaa@gmail.com>
2026-06-23 11:25:29 +0530

    fix folder structure for subdomain layout changes

--- multi-tenent-app-clerk/app/(subdomain)/layout.tsx
+++ multi-tenent-app-clerk/app/(subdomain)/layout.tsx
@@ -1,13 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import {
-  ClerkProvider,
-  Show,
-  SignInButton,
-  SignUpButton,
-  UserButton,
-} from "@clerk/nextjs";
-import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import "../globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,17 +29,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <ClerkProvider>
-          <Show when="signed-out">
-            <SignInButton />
-            <SignUpButton>
-              <button className="bg-purple-700 text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                Sign Up
-              </button>
-            </SignUpButton>
-          </Show>
-          <Show when="signed-in">{children}</Show>
-        </ClerkProvider>
+        <ClerkProvider>{children}</ClerkProvider>
       </body>
     </html>
   );
