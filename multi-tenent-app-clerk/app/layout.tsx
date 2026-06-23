cd4fab344db114bec5eaeaaec86561a1803ac4b6
Visshnnu Tejaa <visshnnutejaa@gmail.com>
2026-06-23 06:58:26 +0530

    Configure clerk

--- multi-tenent-app-clerk/app/layout.tsx
+++ multi-tenent-app-clerk/app/layout.tsx
@@ -1,5 +1,12 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import {
+  ClerkProvider,
+  Show,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +34,22 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <ClerkProvider>
+          <Show when="signed-out">
+            <SignInButton />
+            <SignUpButton>
+              <button className="bg-purple-700 text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                Sign Up
+              </button>
+            </SignUpButton>
+          </Show>
+          <Show when="signed-in">
+            <UserButton />
+            {children}
+          </Show>
+        </ClerkProvider>
+      </body>
     </html>
   );
 }
