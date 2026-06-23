541cbc7e62321301d330e026113a38e9f3b58a0f
Visshnnu Tejaa <visshnnutejaa@gmail.com>
2026-06-23 08:27:22 +0530

    Add Create Post Form using shadcn

--- multi-tenent-app-clerk/app/org/[slug]/page.tsx
+++ multi-tenent-app-clerk/app/org/[slug]/page.tsx
@@ -1,10 +1,32 @@
+"use client";
+
 import Nav from "@/app/components/nav";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { useState } from "react";
 
 const OrgLandingPage = () => {
+  const [blogTitle, setBlogTitle] = useState("");
+  const [blogContent, setBlogContent] = useState("");
   return (
     <main>
       <Nav />
-      <h3>Inside Org Page</h3>
+      <div className="flex justify-center">
+        <div className="p-10 flex flex-col gap-2 w-[600px]">
+          <Input
+            placeholder="Title"
+            value={blogTitle}
+            onChange={(e) => setBlogTitle(e.target.value)}
+          />
+          <Textarea
+            placeholder="Write your blog here..."
+            value={blogContent}
+            onChange={(e) => setBlogContent(e.target.value)}
+          />
+          <Button className="">Create Blog</Button>
+        </div>
+      </div>
     </main>
   );
 };
