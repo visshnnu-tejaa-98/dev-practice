de4d892bf170e32bc4d880550c4c8e500e5b63b2
Visshnnu Tejaa <visshnnutejaa@gmail.com>
2026-06-23 11:49:30 +0530

    Show blogs for specific sub domain

--- multi-tenent-app-clerk/app/(subdomain)/s/[subdomain]/page.tsx
+++ multi-tenent-app-clerk/app/(subdomain)/s/[subdomain]/page.tsx
@@ -1,3 +1,32 @@
-export default function () {
-  return <div>This is a subdomain page</div>;
+import { db } from "@/db";
+import { blogTable } from "@/db/schema";
+import { clerkClient } from "@clerk/nextjs/server";
+import { eq } from "drizzle-orm";
+
+export default async function HomePage({
+  params,
+}: {
+  params: Promise<{ subdomain: string }>;
+}) {
+  const { subdomain } = await params;
+  const client = await clerkClient();
+  const org = await client.organizations.getOrganization({ slug: subdomain });
+  console.log(org);
+  const orgId = org.id;
+
+  const blogs = await db
+    .select()
+    .from(blogTable)
+    .where(eq(blogTable.orgId, orgId));
+
+  return (
+    <div>
+      {blogs.map((blog) => (
+        <div>
+          <h3>{blog.title}</h3>
+          <p>{blog.content}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
